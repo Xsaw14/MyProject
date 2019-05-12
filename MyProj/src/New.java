65dfc6feb9da0e520772527136fbeaff25b05f6b
Xsaw14 <coolvishaalraaj@gmail.com>
2019-05-12 17:15:29 +0530

    Name commit

--- MyProj/src/New.java
+++ MyProj/src/New.java
@@ -4,6 +4,7 @@ public class New {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!!");
+		System.out.println("Hi I'm Vishal");
 	}
 
 }
